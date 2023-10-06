a75ba84939d5eb67546f1dd9bb53e21a18cf8b9f
Ishank Gupta <ishankgupta94482i@gmail.com>
2023-10-06 15:31:55 +0530

    some changes

---
+++ src/utils/messageQueue.js
@@ -0,0 +1,43 @@
+const amqplib = require('amqplib');
+const {MESSAGE_BROKER_URL, EXCHANGE_NAME} = require('../config/serverConfig');
+const createChannel = async () => {
+    try {
+        const conn = await amqplib.connect(MESSAGE_BROKER_URL);
+        const channel = await conn.createChannel();
+        await channel.assertExchange(EXCHANGE_NAME, 'direct', false);
+        return channel;   
+    } catch (error) {
+        throw error;
+    }
+}
+
+const subscribeMessage = async(channel, service, binding_key) => {
+    try {
+        const applicationQueue = await channel.assertQueue('QUEUE_NAME');
+        channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
+        channel.consume(applicationQueue.queue, msg => {
+            console.log('received data');
+            console.log(msg.content.toString());
+            const payload = JSON.parse(msg.content.toString());
+            service(payload);
+            channel.ack(msg);
+        });   
+    } catch (error) {
+        throw error;
+    }
+}
+
+const publishMessage = async(channel, binding_key, message) => {
+    try {
+        await channel.publish(EXCHANGE_NAME, binding_key, message);
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    subscribeMessage,
+    createChannel,
+    publishMessage
+}
